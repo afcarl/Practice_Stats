f6749f007ad381afa81c14b24096cc3d918f598d
nyghtowl <warrick.melanie@gmail.com>
2014-09-07 21:44:38 -0700

    Updated readme about goodness of fit tests. Added code to stats for eda and goodness of fit on dataset. Used ipython for testing code.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 Practice_Stats
 ==============
+
+Evaluating distributions:
+    * Null hypothesis is typically two distributions are the same
+    * Reject the null hypothesis = the two distributions are different
+    * Threshold for p-value analysis typically around 5% and can range between 1-10%
+    * Depends on the distributions being compared and what level is required to confirm difference
+
+[2 Sample Komogorov-Smirnov Test](http://www.physics.csbsju.edu/stats/KS-test.html) (aka: KS Test): 
+    * Determine if two datasets differ significantly. Used for continuous distribution
+    * Does not make assumptions about distributions of data
+    * Non-parametric & distribution free
+    * D stat = max vertical distance between two distributions (smaller is better)
+    * p value = low leads to believing the two distributions are different
+
+[Student's t-Test]() (t-Test):
+    * Determine if two datasets are differ or from same distribution
+    * Handles problems associated iwth inference based on "small" samples 
+    * Accomodate calculated mean could deviate from real mean 
+    * p value = low leads to supporting the two distributions are different
+
+
+[Choosing the Correct Statistical Test Reference](http://www-users.cs.umn.edu/~ludford/stat_overview.htm)
--- stats.py
+++ stats.py
@@ -2,7 +2,128 @@
 Stats Sprint
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import scipy.stats as st
 
+def read_data(filename):
+    f = open(filename)
+    important=['age', 'sex', 'resting_blod_pressuremm_hg', 'cholesterol_mg/dl', 'max_heart_rate']
+    keys = f.readline().strip().split(",")
+    key_to_index = {}
+    for i in range(len(keys)):
+        key_to_index[keys[i]] = i
+    result = []
+    for line in f:
+        line = line.strip().split(",")
+        skip = False
+        for key in important:
+            if line[key_to_index[key]] == '?':
+                skip = True
+        if not skip:
+            line = map(lambda x: None if x == "?" else float(x), line)
+            result.append(line)
+    f.close()
+    return key_to_index, np.array(result)
+
+def age_distributions(loc_data):
+    print "Location Age Histograms"
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+
+    color_vals = ["orange","green","blue", "purple"]
+    
+    for i, (ax, (name, data)) in enumerate(zip(axes.flat,(loc_data))):
+        ax.hist(data[:,0], color=color_vals[i])
+        ax.set_title(name)
+        ax.set_ylabel('age (avg=%d)' % np.mean(data[:,0]))
+        ax.set_xlabel('count')
+    plt.subplots_adjust(wspace = 0.3, hspace = 0.5)
+    plt.show()
+
+
+def boxplots(keys, loc_data):
+    print
+    print "Location Heart Rate & Cholesterol Box Plots"
+    hr = keys['max_heart_rate']
+    chl = keys['cholesterol_mg/dl']
+    male_hr, female_hr, male_chl, female_chl = [], [], [], []
+
+    for (name,data) in loc_data:
+        male_hr = np.append(male_hr, data[data[:,1] == 0, hr], axis=0)
+        female_hr = np.append(female_hr, data[data[:,1] == 1, hr], axis=0)
+        male_chl = np.append(male_chl, data[data[:,1] == 0, chl], axis=0)
+        female_chl = np.append(female_chl, data[data[:,1] == 1, chl], axis=0)
+
+    fig, axes = plt.subplots(ncols=2)
+    axes[0].boxplot((male_hr, female_hr))
+    axes[0].set_title('Heart Rate')
+    axes[0].set_xticklabels(['male', 'female'])
+
+    axes[1].boxplot((male_chl, female_chl))
+    axes[1].set_title('Cholesterol')
+    axes[1].set_xticklabels(['male', 'female'])
+
+    plt.show()
+
+
+def blood_pressure(keys, loc_data):
+    print
+    print "Blood Pressure Histogram Comparison"
+
+    bp = keys['resting_blod_pressuremm_hg']
+    color_vals = ["red","blue"]
+
+    fig, axes = plt.subplots(nrows=4, figsize=(6, 12))
+    for i, (ax, (name, data)) in enumerate(zip(axes.flat,(loc_data))):
+        ax.hist(data[data[:,1] == 1, bp], color="red")
+        ax.hist(data[data[:,1] == 0, bp], color="blue")
+        ax.set_title(name)
+        ax.set_xlabel('blood pressure')
+        ax.legend(['male', 'female'], loc="upper left")
+
+    plt.subplots_adjust(hspace = 1)
+    #plt.show()
+
+def fit_the_line(keys, loc_data):
+    print
+    print "Probability Density Function"
+
+    data = loc_data[0][1]
+    bp = keys['resting_blod_pressuremm_hg']
+    fig, axes = plt.subplots(nrows=4, figsize=(6, 12))
+
+    for i, (ax, (name, data)) in enumerate(zip(axes.flat,(loc_data))):
+        m_blood = data[data[:,1] == 1, bp]
+        f_blood = data[data[:,1] == 0, bp]
+
+        m_blood_mean = np.nanmean(m_blood)
+        f_blood_mean = np.nanmean(f_blood)
+        m_blood_std = np.std(m_blood)     
+        f_blood_std = np.std(f_blood)
+
+        m_count, m_bins, m_ignored = ax.hist(m_blood, 30, color="red", normed=True) 
+        f_count, f_bins, f_ignored = ax.hist(f_blood, 30, color="blue", normed=True)
+
+        m_pdf = st.norm.pdf(m_bins, loc=m_blood_mean, scale=m_blood_std)  
+        f_pdf = st.norm.pdf(f_bins, loc=f_blood_mean, scale=f_blood_std)
+    
+        ax.plot(m_bins, m_pdf, color="green")  
+        ax.plot(f_bins,f_pdf, color="purple")  
+    plt.show()
+
+def check_ks(keys, loc_data):
+    name, data = loc_data
+    x = data[:,3]
+    count, bins, ignore= plt.hist(x, 30, color="green", normed=True)
+    norm_cdf = st.norm.cdf(bins, np.mean(x), np.std(x))
+    data_cdf = np.cumsum(count) * (bins[1] - bins[0])
+
+    d_stat, p_val = st.ks_2samp(data_cdf, norm_cdf)
+    
+    print "The distributions are the same = ", ((d_stat > 0.10) and (p_val < 0.05))
+
+# Need to sync below with above
 def rank_ks():
     cleveland = [c_array[:,2], c_array[:,3], c_array[:,5]]
     labels= ['chest pain', 'resting blood pressure', 'blood sugar']
@@ -11,10 +132,10 @@ def rank_ks():
     for idx, data in enumerate(cleveland):
         count, bin, ignore = hist(data, 30, color="red", normed=True)
         
-        norm_cdf = st.norm.cdf(bin, mean(data), std(data))
+        norm_cdf = st.norm.cdf(bin, np.mean(data), np.std(data))
         data_cdf = cumsum(count) * (bin[1] - bin[0])
         
-        # low ks_stat or high p_val cannot reject hypothesis the distribution of hte two samples are the same
+        # low d_stat or high p_val cannot reject hypothesis - distribution are equal
         ks_stat, p_val = st.ks_2samp(data_cdf, norm_cdf) 
         results[labels[idx]] = (ks_stat, p_val)
 #    print sorted(results.iteritems(), key=lambda x: x[1])
@@ -26,4 +147,23 @@ def rank_ks():
     #plt.imshow([pvals, dvals])
     plt.show()
 
-rank_ks()
+def main():
+    c_keys, c_array = read_data("./data/cleveland_heart.csv")
+    h_keys, h_array = read_data("./data/hungarian_heart.csv")
+    l_keys, l_array = read_data("./data/long_beach.csv")
+    s_keys, s_array = read_data("./data/switzerland_heart.csv")
+    
+    loc_data = [('Cleveland',c_array), ('Hungary', h_array), ('Long Beach', l_array), ('Switzerland',s_array)]
+
+    one_loc_data = ('Cleveland',c_array)
+
+    age_distributions(loc_data)
+    boxplots(c_keys, loc_data)
+    blood_pressure(c_keys, loc_data)
+    fit_the_line(c_keys, loc_data)
+    check_ks(c_keys, one_loc_data)
+    # rank_ks()
+
+
+if __name__ == "__main__":
+    main()
